bf7da36f9711c595a8e30eee6bd71f58541813e5
macrodatum <macrodatum@gmail.com>
2023-04-10 22:53:21 -0500

    add document.js

---
+++ src/pages/_document.js
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Sling Academy</title>
+        <meta
+          name="description"
+          content="System for storage and admin data lakes"
+        />
+        <title>Sling Academy</title>
+        <meta name="title" content="Next Macrodatum System" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
