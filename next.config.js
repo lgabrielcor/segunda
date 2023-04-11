1695c16bba5d59816d4cb641dc09858f88666e1b
macrodatum <macrodatum@gmail.com>
2023-04-10 23:18:15 -0500

    add css clean

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const withCss = require("@zeit/next-css");
+const withPurgeCss = require("next-purgecss");
+module.exports = withCss(withPurgeCss());
+
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
