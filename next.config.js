2313f70b504563e98addc4e9f38e7fe7ec0d04c7
macrodatum <macrodatum@gmail.com>
2023-04-26 21:13:05 -0500

    new screens

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const withCss = require("@zeit/next-css");
 const withPurgeCss = require("next-purgecss");
 module.exports = withCss(withPurgeCss());
+const withPWA = require("next-pwa");
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
 
 const nextConfig = {
   reactStrictMode: true,
